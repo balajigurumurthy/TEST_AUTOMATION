99dc2b554afbc6afa38ec04beb11c1cc00cfe42d
balajigurumurthy <98879563+balajigurumurthy@users.noreply.github.com>
2022-02-03 20:28:03 +1100

    Added function

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,25 @@
 #!bin/bash
+
+echo "========================"
+echo "WELCOME TO TEST FRAMEWORK"
+echo "========================="
+FEATURE_DIR=$1
+TAG=$2
+ROBOT_ARGS=$@
+
+function run_test()
+{
+echo "running test case ...."
+robot . ${FEATURE_DIR} --include  ${TAG} ${ROBOT_ARGS}
+echo "Testing completed"
+}
+
+
+function usage()
+{
+echo "run_test.sh <feature_dir> <tag_name> <robot_args>"
+exit 1
+}
+
+##main 
+run_test ROBOT_ARGS=$@
